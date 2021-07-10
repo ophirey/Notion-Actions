f670a9ff923f3916899cfc1896bf359183e7bacc
Ophir Eyal <ophirey@gmail.com>
2021-07-10 16:38:00 +0300

    fix bug that resulted from passing data as dict instead of json to request

--- utils/request_utils.py
+++ utils/request_utils.py
@@ -1,5 +1,6 @@
+import json
 import os
-from typing import Union
+from typing import Union, Dict
 
 import requests
 import toml as toml
@@ -19,15 +20,32 @@ def get_headers():
     return config["request-headers"]
 
 
-def make_request(url: str, data: Union[str, None]=None):
-    res = requests.post(
+def make_request(request_type: str, url: str, data: Union[Dict, None] = None):
+
+    if request_type == "post":
+        request_callback = requests.post
+    elif request_type == "get":
+        request_callback = requests.get
+    elif request_type == "patch":
+        request_callback = requests.patch
+    else:
+        raise Exception("Invalid Request Type!")
+
+    print(url, data)
+
+    res = request_callback(
         url=url,
         headers=get_headers(),
         auth=BearerAuth(os.environ["NOTION_KEY"]),
-        data=data
+        data=json.dumps(data) if data is not None else {}
     )
+    print(res)
     return res.json()
 
 
-def query_database(database_key_name: str, data: Union[str, None]=None):
-    return make_request(url=f"{os.environ['NOTION_API']}/databases/{os.environ[database_key_name]}/query", data=data)
+def query_database(database_key_name: str):
+    return make_request(request_type="post", url=f"{os.environ['NOTION_API']}/databases/{os.environ[database_key_name]}/query")
+
+
+def update_page(page_key: str, data: Dict):
+    return make_request(request_type="patch", url=f"{os.environ['NOTION_API']}/pages/{page_key.replace('-', '')}", data=data)
--- weekly_reset/weekly_reset.py
+++ weekly_reset/weekly_reset.py
@@ -1,7 +1,7 @@
 from typing import Dict, List
 from datetime import datetime, timedelta
 
-from utils.request_utils import query_database
+from utils.request_utils import query_database, update_page
 
 
 def get_previous_week():
@@ -11,24 +11,26 @@ def get_previous_week():
 def update_previous_week(pages_to_update: List[str], properties: Dict[str, str]):
     reset_mapper = lambda iso_date: {
         "checkbox": {"checkbox": False},
-        "rich_text": [{"text": {"content": ""}}],
+        "rich_text": {"rich_text": [{"text": {"content": ""}}]},
         "date": {"date": {"start": iso_date}}
     }
 
-    for num_of_days, page in enumerate(pages_to_update):
-        date = (datetime.now() + timedelta(days=num_of_days)).isoformat()
+    for num_of_days, page_id in enumerate(pages_to_update):
+        date = (datetime.now() + timedelta(days=num_of_days)).strftime("%Y-%m-%d")
         reset_config = reset_mapper(date)
         data = {
             "properties": {
                 k: reset_config[properties[k]] for k in properties
             }
         }
-        print(data)
+        update_page(page_id, data)
+
 
 if __name__ == '__main__':
+    prev_week_db = get_previous_week()
     print(
         update_previous_week(
-            ["1", "2", "3"],
+            [page["id"] for page in prev_week_db["results"]],
             {
                 "Date": "date",
                 "Comments": "rich_text",
